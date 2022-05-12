e0774264e903497227994facf2a69f11e40cdb54
supriyobhattacherjee <supriyo.is4u@gmail.com>
2022-05-12 18:35:43 +0530

    Create SharepointonnectUtil.cs

---
+++ SharepointonnectUtil.cs
@@ -0,0 +1,17 @@
+private async Task GetClientContextWithAccessToken1(string targetUrl)
+{
+    var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new string[] { "https://*****.sharepoint.com/Sites.Manage.All" });
+
+    using(ClientContext clientContext = new ClientContext(targetUrl))
+    {
+        clientContext.ExecutingWebRequest +=
+            delegate (object oSender, WebRequestEventArgs webRequestEventArgs)
+            {
+                webRequestEventArgs.WebRequestExecutor.RequestHeaders["Authorization"] =
+                    "Bearer " + accessToken;
+            };
+        List list = clientContext.Web.Lists.GetByTitle("BeoneTestDocLibrary");
+        clientContext.Load(list);
+        clientContext.ExecuteQuery();
+    }
+}
